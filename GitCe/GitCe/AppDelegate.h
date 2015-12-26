31f50a3dfae99b7e468376ac8c17cdc9808b4df3
Ibokan <ibokan@11-11.local>
2015-12-26 19:20:04 +0800

    这样可以吗

---
+++ GitCe/GitCe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCe
+//
+//  Created by Ibokan on 15/12/26.
+//  Copyright (c) 2015年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCe/GitCe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitCe
+//
+//  Created by Ibokan on 15/12/26.
+//  Copyright (c) 2015年 Ibokan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
